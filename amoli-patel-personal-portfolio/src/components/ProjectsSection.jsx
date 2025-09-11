f95df70fb2e034d6722775954b7fe98774a0adff
amolipatel05 <patel.amol@northeastern.edu>
2025-09-11 15:42:39 -0400

    Updated styling of Good Dog Licensing project card

--- amoli-patel-personal-portfolio/src/components/ProjectsSection.jsx
+++ amoli-patel-personal-portfolio/src/components/ProjectsSection.jsx
@@ -7,20 +7,20 @@ export const ProjectsSection = () => {
       className="scroll-mt-28 relative min-h-screen flex flex-col items-center justify-center px-4"
     >
       <div className="grid grid-cols-1 gap-6 mx-auto justify-items-center bg-background py-8 dark:bg-background-dark mdlg:grid-cols-2 xl:grid-cols-3 w-fit">
-        <div className="shadow-2xl bg-gray-200 h-140 w-80 rounded-lg dark:bg-gray-300">
-          <h1 className="pt-4 font-semibold text-xl"> Good Dog Licensing</h1>
+        <div className="shadow-2xl bg-primary/10 h-140 w-80 rounded-lg ">
+          <h1 className="pt-4 font-semibold text-xl "> Good Dog Licensing</h1>
           <div className="p-4">
             <img
               src="/images/gdl.png"
               alt="Good Dog Licensing"
               className="overflow-hidden rounded-xl pb-2 card-hover"
             ></img>
-            <div className="bg-gray-200 rounded-xl p-2 align">
+            <div className="bg-primary/10 rounded-xl p-2 align">
               <p className="text-center">
                 React, Next.js, TypeScript, tRPC, Prisma, TailwindCSS
               </p>
             </div>
-            <p className="p-2">
+            <p className="p-2 ">
               A full-stack web application automates the music licensing process
               for Northeastern’s Green Line Records, improving the current
               manual process for musicians and media makers to create accounts,
@@ -37,7 +37,7 @@ export const ProjectsSection = () => {
                 {" "}
                 <div className="shadow-xl bg-gray-400 p-2 rounded-xl hover:scale-110 transition-transform duration-300 flex justify-center">
                   <Github className="h-6 w-6 text-black" />
-                  <p>Visit Github</p>
+                  <p className="">Visit Github</p>
                 </div>
               </a>
               {/* Open website for Good Dog Licensing */}
@@ -49,17 +49,17 @@ export const ProjectsSection = () => {
                 {" "}
                 <div className="shadow-xl bg-gray-400 p-2 rounded-xl hover:scale-110 transition-transform duration-300 flex justify-center">
                   <ExternalLink className="h-6 w-6 text-black" />
-                  <p>Visit Site</p>
+                  <p className="">Visit Site</p>
                 </div>
               </a>
             </div>
           </div>
         </div>
 
-        <div className="bg-gray-200 h-96 w-60 rounded-lg dark:bg-gray-300"></div>
-        <div className="bg-gray-200 h-96 w-60 rounded-lg dark:bg-gray-300"></div>
-        <div className="bg-gray-200 h-96 w-60 rounded-lg dark:bg-gray-300"></div>
-        <div className="bg-gray-200 h-96 w-60 rounded-lg dark:bg-gray-300"></div>
+        <div className="bg-primary/10 h-96 w-60 rounded-lg"></div>
+        <div className="bg-gray-200 h-96 w-60 rounded-lg dark:bg-gray-700"></div>
+        <div className="bg-gray-200 h-96 w-60 rounded-lg dark:bg-gray-700"></div>
+        <div className="bg-gray-200 h-96 w-60 rounded-lg dark:bg-gray-700"></div>
       </div>
     </section>
   );
